62ff31a4f888ddf240811be904152630c3cf4a66
yazoink <yazoink@firemail.cc>
2024-10-20 19:54:06 +1100

    fix thing

--- js/rss.js
+++ js/rss.js
@@ -1,5 +1,3 @@
-// this is kinda useless without a web server
-
 function addLeadingZero(num) {
     num = num.toString();
     while (num.length < 2) num = "0" + num;
@@ -7,36 +5,37 @@ function addLeadingZero(num) {
 }
 
 function dateToRSS(dateString) {
+  const [dayString, monthString, yearString] = dateString.split('/');
+  const date = new Date(yearString, monthString - 1, dayString);
   const dayStrings = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   const monthStrings = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-  const timeStamp = Date.parse(dateString);
-  const date = new Date(timeStamp);
-  const day = dayStrings[date.getDay()];
-  const dayNumber = addLeadingZero(date.getDate());
+  const dayOfWeek = dayStrings[date.getDay()];
+  const dayOfMonth = addLeadingZero(date.getDate());
   const month = monthStrings[date.getMonth()];
   const year = date.getFullYear();
   const hours = addLeadingZero(date.getHours());
   const minutes = addLeadingZero(date.getMinutes());
   const seconds = addLeadingZero(date.getSeconds());
-  const timezoneOffset = "AEST";
-  return `${day}, ${dayNumber} ${month} ${year} ${hours}:${minutes}:${seconds} ${timezone}`;
+  const timezone = "AEST";
+  return `${dayOfWeek}, ${dayOfMonth} ${month} ${year} ${hours}:${minutes}:${seconds} ${timezone}`;
 }
 
-function outputRSS(blog) {
+function getRSS(blog) {
   let rssXML = `<?xml version="1.0" encoding="UTF-8"?>
   <rss version="2.0">
   <channel>
-    <title>${rssFeedInfo.blogTitle}</title>
+    <title>Potatocat</title>
     <link>https://${window.location.hostname}</link>
-    <description>${rssFeedInfo.blogDescription}</description>
+    <description>Potatocat's blog</description>
     `;
   for (var i = 0; i < blog.length; i++) {
-    var postDate = dateToRSS(blog[i].date);
+    var entry = blog[i].title.replace(/ /g, "-");
+    entry = entry.toLowerCase();
+    var postDate = dateToRSS(blog[i].datePublished);
     rssXML += `
       <item>
         <title>${blog[i].title}</title>
-        <link>https://${window.location.hostname}/index.html?nav=blog&entry=${blog[i].entry}</link>
+        <link>https://${window.location.hostname}/index.html?nav=blog&entry=${entry}</link>
         <description>${blog[i].subheading}</description>
         <pubDate>${postDate}</pubdate>
       </item>
