62749dd457cc1e42d9def44d537ad4d088a446de
yazoink <yazoink@firemail.cc>
2024-10-20 18:09:39 +1100

    fix thing

--- js/get.js
+++ js/get.js
@@ -1,6 +1,9 @@
-var parts = window.location.search.substr(1).split("&");
-var get = {};
-for (var i = 0; i < parts.length; i++) {
-  var temp = parts[i].split("=");
-  get[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1]);
+function getGET(url) {
+  var parts = url.search.substr(1).split("&");
+  var GET = {};
+  for (var i = 0; i < parts.length; i++) {
+    var temp = parts[i].split("=");
+    GET[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1]);
+  }
+  return GET;
 }
