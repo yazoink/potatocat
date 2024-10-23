6499bb19ef3d3e61845c27b599633032cfba8a8b
yazoink <yazoink@firemail.cc>
2024-10-24 01:42:51 +1100

    add prev/next links to gallery

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,10 +1,24 @@
 const artGallery = {
   "subheading": "This is gonna be a gallery or smth",
-  "description": "Click on images for full view."
+  "description": "Click on images for full view.",
+  "maxPerPage": 6 // max images per page
 }
 
 const artGalleryImages = [
   "art1.webp",
   "art2.webp",
-  "art3.webp"
+  "art3.webp",
+  "art4.webp",
+  "art5.webp",
+  "art6.webp",
+  "art7.webp",
+  "art8.webp",
+  "art9.webp",
+  "art10.webp",
+  "art11.webp",
+  "art12.webp",
+  "art13.webp",
+  "art14.webp",
+  "art15.webp",
+  "art16.webp"
 ]
