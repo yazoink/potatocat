9e54fb3ef48cae751b8159fcb95fc8a1826bb70a
yazoink <yazoink@firemail.cc>
2024-10-24 15:51:51 +1100

    tweak thing

--- js/get.js
+++ js/get.js
@@ -1,8 +1,8 @@
 function getGET(url) {
-  var parts = url.search.substr(1).split("&");
-  var GET = {};
-  for (var i = 0; i < parts.length; i++) {
-    var temp = parts[i].split("=");
+  const parts = url.search.substr(1).split("&");
+  let GET = {};
+  for (let i = 0; i < parts.length; i++) {
+    let temp = parts[i].split("=");
     GET[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1]);
   }
   return GET;
